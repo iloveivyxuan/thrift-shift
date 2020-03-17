441a2cc64a761f17fe39f2d878510ebfcfa4bd32
ivyxuan <iloveivyxuan@gmail.com>
2020-03-17 18:04:09 -0500

    Fix travis command

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Thrift Shift link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText('Thrift Shift');
   expect(linkElement).toBeInTheDocument();
